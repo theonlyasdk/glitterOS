c608c8c75709499f20a08ce787b5d4b6de1d400f
ASDK <168300808+theonlyasdk@users.noreply.github.com>
2025-12-28 22:18:03 +0530

    Transition from LilacDE to glitterOS

--- core/lilac.js
+++ core/lilac.js
@@ -1,6 +1,14 @@
 const menubar = document.getElementById("menubar");
 const desktop = document.getElementById("desktop");
 const dock = document.getElementById("dock");
+
+const alertDialog = document.getElementById("alert-dialog");
+const alertDialogTitle = document.getElementById("alert-title");
+const alertDialogBody = document.getElementById("alert-body");
+const alertDialogBtnClose = document.getElementById("alert-btn-close");
+const alertDialogBtn1 = document.getElementById("alert-btn-1");
+const alertDialogBtn2 = document.getElementById("alert-btn-2");
+
 const applets = {
 	date: {
 		elem: document.getElementById("lde-mbar-applet-date"),
@@ -12,6 +20,38 @@ const applets = {
 	}
 }
 
+function showConfirm(title,msg,on_confirm) {
+	showDialog(title,msg,null,{
+		btn1: {
+			text:"OK",
+			onclick:on_confirm,
+		},
+	})
+}
+
+function showDialog(title,msg,warnlevel=null,extrabtns=null) {
+	// hide buttons initially
+	alertDialogBtn1.style.display="none";
+	alertDialogBtn2.style.display="none";
+
+	if (extrabtns) {
+		if (extrabtns.btn1) {
+			alertDialogBtn1.style.display="block";
+			alertDialogBtn1.innerHTML=extrabtns.btn1.text;
+			alertDialogBtn1.onclick=extrabtns.btn1.onclick;
+		}
+		if (extrabtns.btn2) {
+			alertDialogBtn2.style.display="block";
+			alertDialogBtn2.innerHTML=extrabtns.btn2.text;
+			alertDialogBtn2.onclick=extrabtns.btn2.onclick;	
+		}
+	}
+
+	const dialog = new bootstrap.Modal(alertDialog, {});
+	alertDialogTitle.innerHTML=title;
+	alertDialogBody.innerHTML=msg;
+	dialog.show();
+}
 function assertExistsElseReload(elem) {
 	if (!elem) {
 		window.alert(elem + " does not exist! Reloading the window in 5s...");
@@ -51,5 +91,15 @@ function ldeInit() {
 }
 function kaboom(sender) {
 	document.body.classList.add("kaboom");
+}
+function aboutGlitterOS() {
+	const msg = `
+		<b>glitterOS</b> is an operating system that runs inside your browser.
+		<br>
+		<br>
+		<i class="d-inline-block text-center w-100">Have fun!!!!</i>
+		<small class="w-100 text-center d-inline-block text-secondary">(c) theonlyasdk 2025-26</small>
+	`;
+	showDialog("About glitterOS",msg);
 }
 ldeInit();
